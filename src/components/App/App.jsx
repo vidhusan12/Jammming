38872657f72a69e2eaf4dbbf3c7b40cc2544369f
vidhusan12 <vidhusan709@gmail.com>
2025-06-02 22:59:32 +1200

    Display track

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
+
+
 function App() {
+  
+  const [searchResults, setSearchResults] = useState([
+    { id: 1, name: 'Song A', artist: 'Artist A', album: 'Album A' },
+    { id: 2, name: 'Song B', artist: 'Artist B', album: 'Album B' }
+  ]);
   return (
     <div>
       <h1>Jammming</h1>
       <SearchBar />
       <div style={{ display: 'flex', gap: '2rem' }}>
-        <SearchResults />
+        <SearchResults searchResults={searchResults}/>
         <Playlist />
       </div>
     </div>
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Tracklist from '../Tracklist/Tracklist';
+import Track from '../Track/Track';
 
-function SearchResults() {
+function SearchResults({ searchResults }) {
+  const trackComponents = searchResults.map(track => {
+    return (
+      <Track
+        key={track.id}
+        name={track.name}
+        artist={track.artist}
+        album={track.album}
+      />
+    );
+  });
   return (
     <div>
       <h2>Results</h2>
-      <Tracklist />
+      <Tracklist>{trackComponents}</Tracklist>
     </div>
   );
 }
