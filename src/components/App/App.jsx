747145196c0824baaba95cfe4eef4f64b97489e4
vidhusan12 <vidhusan709@gmail.com>
2025-06-03 18:20:24 +1200

    Implement mock save playlist functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,15 +6,16 @@ import Playlist from '../Playlist/Playlist';
 
 function App() {
   const [searchResults, setSearchResults] = useState([
-    { id: 1, name: 'Song A', artist: 'Artist A', album: 'Album A' },
-    { id: 2, name: 'Song B', artist: 'Artist B', album: 'Album B' },
-    { id: 3, name: 'Song C', artist: 'Artist C', album: 'Album C' }
+    { id: 1, name: 'Song A', artist: 'Artist A', album: 'Album A', uri: 'spotify:track:abc123' },
+    { id: 2, name: 'Song B', artist: 'Artist B', album: 'Album B', uri: 'spotify:track:def456' },
+    { id: 3, name: 'Song C', artist: 'Artist C', album: 'Album C', uri: 'spotify:track:ghi789' }
   ]);
 
   const [playlistName, setPlaylistName] = useState("My Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([
-    { id: 1, name: 'Song A', artist: 'Artist A', album: 'Album A' },
-    { id: 2, name: 'Song B', artist: 'Artist B', album: 'Album B' }
+    { id: 1, name: 'Song A', artist: 'Artist A', album: 'Album A', uri: 'spotify:track:abc123' },
+    { id: 2, name: 'Song B', artist: 'Artist B', album: 'Album B', uri: 'spotify:track:def456' },
+    { id: 3, name: 'Song C', artist: 'Artist C', album: 'Album C', uri: 'spotify:track:ghi789' }
   ]);
 
   function addTrack(track) {
@@ -25,6 +26,14 @@ function App() {
 
   function removeTrack(track) {
     setPlaylistTracks(playlistTracks.filter(savedTrack => savedTrack.id !== track.id));
+  }
+
+  function savePlaylist() {
+    const trackUris = playlistTracks.map(track => track.uri);
+    console.log("Saving playlist with URIs: ", trackUris);
+    setPlaylistName("New Playlist"); //resets the playlsit
+    setPlaylistTracks([]); //resets the playlist Track
+
   }
 
   return (
@@ -41,6 +50,7 @@ function App() {
           tracks={playlistTracks}
           isRemoval={true}
           removeTrack={removeTrack}
+          savePlaylist={savePlaylist}
         />
       </div>
     </div>
--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from './Playlist.module.css';
 import Tracklist from '../Tracklist/Tracklist';
 
-function Playlist({ playlistName, setPlaylistName, tracks, isRemoval, removeTrack }) {
+function Playlist({ playlistName, setPlaylistName, tracks, isRemoval, removeTrack, savePlaylist }) {
   return (
     <div className={styles.Playlist}>
       <input onChange={e => setPlaylistName(e.target.value)} value={playlistName} />
       <Tracklist tracks={tracks} isRemoval={isRemoval} removeTrack={removeTrack} />
-      <button className={styles['Playlist-save']}>Save To Spotify</button>
+      <button onClick={savePlaylist} className={styles['Playlist-save']}>Save To Spotify</button>
     </div>
   );
 }
